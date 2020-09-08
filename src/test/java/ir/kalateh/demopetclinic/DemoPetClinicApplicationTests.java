1a1dd0507ab13fbcb940463eaf2484973444004a
hossein <h.kalateh72@gmail.com>
2020-09-09 02:34:18 +0430

    initial commit and start project

---
+++ src/test/java/ir/kalateh/demopetclinic/DemoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.kalateh.demopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPetClinicApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
