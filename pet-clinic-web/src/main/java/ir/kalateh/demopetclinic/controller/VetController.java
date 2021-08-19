bd5a95572333a98fdd630e3577f7942355bf2c66
hossein <h.kalateh72@gmail.com>
2021-08-20 03:31:58 +0430

    add list of vets in vets index, closes #17.

--- pet-clinic-web/src/main/java/ir/kalateh/demopetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/ir/kalateh/demopetclinic/controller/VetController.java
@@ -1,14 +1,23 @@
 package ir.kalateh.demopetclinic.controller;
 
+import ir.kalateh.demopetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("vets")
 public class VetController {
     
+    private final VetService vetService;
+    
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+    
     @RequestMapping({"", "/", "index", "index.html"})
-    public String vet() {
+    public String vet(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         
         return "vet/index";
     }
