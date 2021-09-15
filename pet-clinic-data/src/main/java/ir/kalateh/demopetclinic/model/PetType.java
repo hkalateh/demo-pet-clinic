7be649b3263e46bcbf6296ca13170f45d2afd626
hossein <h.kalateh72@gmail.com>
2021-09-16 01:45:39 +0430

    remove additional getter and setters

--- pet-clinic-data/src/main/java/ir/kalateh/demopetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ir/kalateh/demopetclinic/model/PetType.java
@@ -10,12 +10,4 @@ import javax.persistence.Entity;
 @Entity
 public class PetType extends BaseEntity {
     private String name;
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
 }
