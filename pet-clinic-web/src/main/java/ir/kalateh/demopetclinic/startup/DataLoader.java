9a5c551902f2c1bdeaab55dca1bc3ecbeb85b43d
hossein <h.kalateh72@gmail.com>
2021-09-27 19:12:26 +0330

    add pet type to startup, close #36

--- pet-clinic-web/src/main/java/ir/kalateh/demopetclinic/startup/DataLoader.java
+++ pet-clinic-web/src/main/java/ir/kalateh/demopetclinic/startup/DataLoader.java
@@ -1,9 +1,11 @@
 package ir.kalateh.demopetclinic.startup;
 
 import ir.kalateh.demopetclinic.model.Owner;
+import ir.kalateh.demopetclinic.model.PetType;
 import ir.kalateh.demopetclinic.model.Speciality;
 import ir.kalateh.demopetclinic.model.Vet;
 import ir.kalateh.demopetclinic.services.OwnerService;
+import ir.kalateh.demopetclinic.services.PetTypeService;
 import ir.kalateh.demopetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,14 +18,26 @@ public class DataLoader implements CommandLineRunner {
     
     private final VetService vetService;
     private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
     
-    public DataLoader(VetService vetService, OwnerService ownerService) {
+    public DataLoader(VetService vetService, OwnerService ownerService, PetTypeService petTypeService) {
         this.vetService = vetService;
         this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
     }
     
     @Override
     public void run(String... args) throws Exception {
+    
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+    
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+    
+        System.out.println("#pet types : " + petTypeService.findAll().size());
     
         Owner owner1 = new Owner();
         owner1.setFirstName("salman");
@@ -36,9 +50,8 @@ public class DataLoader implements CommandLineRunner {
         owner.setLastName("kalateh");
         
         ownerService.save(owner);
-    
-        System.out.println("owners loaded....");
-        System.out.println((long) ownerService.findAll().size());
+        
+        System.out.println("#owners : " + (long) ownerService.findAll().size());
     
         Speciality speciality = new Speciality();
         speciality.setDescription("ghalb");
@@ -57,10 +70,9 @@ public class DataLoader implements CommandLineRunner {
         vet1.setLastName("maghsoudi");
         
         vetService.save(vet1);
+        
+        System.out.println("number of saved vets : " + (long) vetService.findAll().size());
     
-        System.out.println("vets loaded...");
-        System.out.println((long) vetService.findAll().size());
-    
-        System.out.println(vet.getSpecialities().size());
+        System.out.println("#vet's Specialities : " + vet.getSpecialities().size());
     }
 }
